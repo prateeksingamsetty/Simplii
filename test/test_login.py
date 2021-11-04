68c750ae340fbb163ea2404dc7249e1bbd4ff102
Sunidhi Hegde <62884191+SunidhiHegde@users.noreply.github.com>
2021-11-04 11:28:40 -0400

    Update test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -12,4 +12,8 @@ class TestModule(unittest.TestCase):
         response = tester.get("/login", content_type='html/text')
         self.assertEqual(response.status_code, 200)
         
+        
+if __name__ == "__main__":
+    unittest.main()
+        
     
