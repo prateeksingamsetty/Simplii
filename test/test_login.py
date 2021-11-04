5875615b86ec17267903cfb749a47432514a88f6
Sunidhi Hegde <62884191+SunidhiHegde@users.noreply.github.com>
2021-11-04 11:28:02 -0400

    Update test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -1,1 +1,15 @@
+import unittest
+from src.app import app
+import requests
 
+
+app.testing = True
+
+class TestModule(unittest.TestCase):
+        
+    def test_login(self):
+        tester = app.test_client(self)
+        response = tester.get("/login", content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        
+    
