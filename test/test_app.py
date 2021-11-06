4a7a6f5ba3d501f849fe91cf570f83a7083d1db0
radhikaraman20 <89432698+radhikaraman20@users.noreply.github.com>
2021-11-05 23:05:56 -0400

    Update

--- test/test_app.py
+++ test/test_app.py
@@ -16,13 +16,13 @@ class TestModule(unittest.TestCase):
         tester = app.test_client(self)
         inp = {"id": "7X9UE", "task_name": "dummy_task", "deadline": "2021-03-30 21:08", "estimate": 1, "task_type": "physical", "quant_verbal": "NA", "creat_consum": "NA", "difficulty": "3", "task_status":"Pending"}
         response = requests.post("http://127.0.0.1:5000/add_task", data=inp)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 500)
 
     def test_delete_task(self):
         tester = app.test_client(self)
         inp = {"id": "23"}
         response = requests.post("http://127.0.0.1:5000/delete_task", data=inp)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 500)
 
    
 
